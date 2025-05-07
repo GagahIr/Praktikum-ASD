1aabced271c0c2bcdc760c3b6f67d580c27d4d10
GagahIr <gagahirlangsa30@gmail.com>
2025-05-07 15:49:06 +0700

    Jobsheet 10

---
+++ P2Josbheet10/Mahasiswa.java
@@ -0,0 +1,16 @@
+package P2Josbheet10;
+
+public class Mahasiswa {
+    String nim, nama, prodi, kelas;
+
+    Mahasiswa(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    void tampilData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
+}
